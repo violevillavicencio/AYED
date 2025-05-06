1db814206bb99e995d740f7c05defecc3945b09b
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2025-05-06 20:55:21 -0300

    Create SumaYDif.java

---
+++ practica-2/ejercicio09 /SumaYDif.java
@@ -0,0 +1,23 @@
+package practica2.ejercicio09;
+
+public class SumaYDif {
+    private int suma; 
+    private int dif; 
+    
+    public int getSuma() {
+        return suma;
+    }
+
+    public void setSuma(int sum) {
+        this.suma = sum;
+    }
+
+    public int getDif() {
+        return dif;
+    }
+
+    public void setDif(int dif) {
+        this.dif = dif;
+    }
+    
+}
